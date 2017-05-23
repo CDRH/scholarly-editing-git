1e04ff4b08378fa22b3210a274709f7a443874b5
karindalziel <kdalziel2@unl.edu>
2017-05-23 15:04:30 -0500

    PDF's and other issues

--- 2016/editions/Dunning_De-oratione-dominica/js/dunning.js
+++ 2016/editions/Dunning_De-oratione-dominica/js/dunning.js
@@ -15,145 +15,123 @@ $.fn.clicktoggle = function(a, b) {
     });
 };
 
-/*$(function () {
-$("#dunning_controls_expan").text("Hide Editorial Expansions");
-    $("#dunning_controls_expan").clicktoggle(
-        function () {
-            $(this).text("Show Editorial Expansions");
-            $(".tei_ex").hide();
-        },
-        function () {
-            $(this).text("Hide Editorial Expansions");
-            $(".tei_ex").show();
-        }
-    );
-});*/
 
-$(function () {
-$(".highlight_quotes").clicktoggle(
-    function () {
-        event.preventDefault();
-        $(this).text("Unhighlight Quotes");
-        $(".quote").addClass("highlight");
-        $(this).addClass("selected");
-    },
-    function () {
-        event.preventDefault();
-        $(this).text("Highlight Quotes");
-        $(".quote").removeClass("highlight");
-        $(this).removeClass("selected");
-    }
-);
+$( document ).ready(function() {
+  $(".highlight_quotes").clicktoggle(
+      function () {
+          $(this).text("Unhighlight Quotes");
+          $(".quote").addClass("highlight");
+          $(this).addClass("selected");
+          event.preventDefault();
+
+      },
+      function () {
+          $(this).text("Highlight Quotes");
+          $(".quote").removeClass("highlight");
+          $(this).removeClass("selected");
+          event.preventDefault();
+    
+      }
+  );
 }); 
 
 
 
-$(function () {
-    $(".line_breaks").clicktoggle(
+$( document ).ready(function() {
+  $(".line_breaks").clicktoggle(
+      function () {
+          
+          $(this).text("Enable Line Breaks");
+          $("br").addClass("br_regularized");
+          $("br").removeClass("br_diplomatic");
+          $(this).addClass("selected");
+          $(".tei_lb, .tei_pb_label").hide();
+          event.preventDefault();
+   
+      },
+      function () {
+          $(this).text("Disable Line Breaks");
+          $("br").addClass("br_diplomatic");
+          $("br").removeClass("br_regularized");
+          $(this).removeClass("selected");
+          $(".tei_lb, .tei_pb_label").show();
+          event.preventDefault();
+     
+      }
+  );
+});
+
+$( document ).ready(function() {
+  $(".tei_hand_attr_B").hide();
+    $(".handwritten_text").clicktoggle(
         function () {
-            event.preventDefault();
-            $(this).text("Enable Line Breaks");
-            $("br").addClass("br_regularized");
-            $("br").removeClass("br_diplomatic");
+            
+            $(this).text("Hide Handwritten Text");
+            $(".tei_hand_attr_B").show();
             $(this).addClass("selected");
-            $(".tei_lb, .tei_pb_label").hide();
+            event.preventDefault();
         },
         function () {
-            event.preventDefault();
-            $(this).text("Disable Line Breaks");
-            $("br").addClass("br_diplomatic");
-            $("br").removeClass("br_regularized");
+            
+            $(this).text("Display Handwritten Text");
+            $(".tei_hand_attr_B").hide();
             $(this).removeClass("selected");
-            $(".tei_lb, .tei_pb_label").show();
-        }
-    );
-});
-
-$(function () {
-$(".tei_hand_attr_B").hide();
-$(".handwritten_text").clicktoggle(
-    function () {
-        event.preventDefault();
-        $(this).text("Hide Handwritten Text");
-        $(".tei_hand_attr_B").show();
-        $(this).addClass("selected");
-    },
-    function () {
-        event.preventDefault();
-        $(this).text("Display Handwritten Text");
-        $(".tei_hand_attr_B").hide();
-        $(this).removeClass("selected");
-    }
-);
+            event.preventDefault();
+      }
+  );
 }); 
 
-$(function () {
-$("#highlight_quotes_note").hide();
-$("#highlight_quotes_link").clicktoggle(
-    function () {
-        event.preventDefault();
-        $("#highlight_quotes_note").show();
-        $("#line_breaks_note").hide();
-        $("#handwritten_text_note").hide();
-    },
-    function () {
-        event.preventDefault();
-        $("#highlight_quotes_note").hide();
-    }
-    );
-});
 
-$(function () {
-$("#line_breaks_note").hide();
-$("#line_breaks_link").clicktoggle(
-    function () {
-        event.preventDefault();
-        $("#line_breaks_note").show();
-        $("#highlight_quotes_note").hide();
-        $("#handwritten_text_note").hide();
-    },
-    function () {
-        event.preventDefault();
-        $("#line_breaks_note").hide();
-    }
-    );
-});
-
-$(function () {
-$("#handwritten_text_note").hide();
-$("#handwritten_text_link").clicktoggle(
-    function () {
-        event.preventDefault();
-        $("#handwritten_text_note").show();
-        $("#line_breaks_note").hide();
-        $("#highlight_quotes_note").hide();
-    },
-    function () {
-        event.preventDefault();
-        $("#handwritten_text_note").hide();
-        
-    }
-    );
-});
-
-
-
-/*$(function () {
-    $(".translation_links").clicktoggle(
+$( document ).ready(function() {
+  $("#highlight_quotes_note").hide();
+    $("#highlight_quotes_link").clicktoggle(
         function () {
+            
+            $("#highlight_quotes_note").show();
+            $("#line_breaks_note").hide();
+            $("#handwritten_text_note").hide();
+            event.preventDefault();
+        },
+        function () {
+            $("#highlight_quotes_note").hide();
+            event.preventDefault();
+        }
+  );
+});
+
+$( document ).ready(function() {
+  $("#line_breaks_note").hide();
+    $("#line_breaks_link").clicktoggle(
+        function () {
+            
+            $("#line_breaks_note").show();
+            $("#highlight_quotes_note").hide();
+            $("#handwritten_text_note").hide();
+            event.preventDefault();
+        },
+        function () {
+            
+            $("#line_breaks_note").hide();
+            event.preventDefault();
+      }
+    );
+});
+
+$( document ).ready(function() {
+  $("#handwritten_text_note").hide();
+    $("#handwritten_text_link").clicktoggle(
+        function () {
+            $("#handwritten_text_note").show();
+            $("#line_breaks_note").hide();
+            $("#highlight_quotes_note").hide();
+            event.preventDefault();
+        },
+        function () {
+            $("#handwritten_text_note").hide();
             event.preventDefault();
             
-            $(this).text("Show Translation Links");
-            $(".tei_seg").removeClass('tei_seg').addClass('tei_seg_off');
-            $(".orig_trans_link").hide();
-            $(".translation_links").addClass("selected");
-        },
-        function () {
-            event.preventDefault();
-             $(this).text("Hide Translation Links");
-            $(".tei_seg_off").addClass('tei_seg').removeClass('tei_seg_off');
-            $(".orig_trans_link").show();
-            $(".translation_links").removeClass("selected");
         }
-    );
-});*/
+      );
+});
+
+
